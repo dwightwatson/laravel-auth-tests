d098c731e26fea803166c2e5bc8ac9039d412965
Alfredo Eka <alfredoeka@outlook.com>
2018-11-12 03:22:51 +0700

    Exclude VerifyCsrfToken middleware

---
+++ TestCase.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests;
+
+use App\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->withoutMiddleware(VerifyCsrfToken::class);
+    }
+}
